509d5e39357f39ebfd62d2681c6931c24af06c55
Yigit S <55548182+yigitsr@users.noreply.github.com>
2024-07-09 10:10:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function backColor(){
 function textColor(){
 	_divID.classList.toggle('text-color');
 }
+
+
+//add
+_divID.classList.add('addClass')
+
+//remove
+_divID.classList.remove('removeClass')
+
+//contains will return true if it has class of 'foo' or false if it does not
+_divID.classList.contains('containsClass')
