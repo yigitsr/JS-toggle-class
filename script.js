b8cbab7bfae0a89c1166d03b2172f593128eeb83
Yigit S <55548182+yigitsr@users.noreply.github.com>
2024-07-09 10:04:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const _divID = document.getElementById('divID');
+
+
+function hideNSeek(){
+	_divID.classList.toggle('visible');
+}
+
+function backColor(){
+	_divID.classList.toggle('bg-color');
+}
+
+function textColor(){
+	_divID.classList.toggle('text-color');
+}
